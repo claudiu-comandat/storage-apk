9dec7305ec0e31c907f271ded1e03dcb4f1cf0c3
claudiu-comandat <claudiu.tita2022@gmail.com>
2026-06-21 20:49:45 +0300

    Update GET_ORDERS_WEBHOOK_URL to new endpoint

--- www/scripts/state.js
+++ www/scripts/state.js
@@ -1,7 +1,7 @@
 // --- Constante API ---
 window.STORAGE_WEBHOOK_URL = "https://automatizare.comandat.ro/webhook/storage-update";
 window.GET_STORAGE_WEBHOOK_URL = "https://automatizare.comandat.ro/webhook/get-storage";
-window.GET_ORDERS_WEBHOOK_URL = "https://automatizare.comandat.ro/webhook/8ba5359d-8ecd-4576-b44c-934ac4b661e2";
+window.GET_ORDERS_WEBHOOK_URL = "https://processes.comandat.ro/webhook/8ba5359d-8ecd-4576-b44c-934ac4b661e2";
 window.REFRESH_TOKEN_WEBHOOK = "https://automatizare.comandat.ro/webhook/refresh-easysales-print-access";
 window.PRINT_AWB_WEBHOOK = "https://automatizare.comandat.ro/webhook/print-awb-easysales";
 window.GENERATE_AWB_WEBHOOK = "https://automatizare.comandat.ro/webhook/generate-awb-easysales";
